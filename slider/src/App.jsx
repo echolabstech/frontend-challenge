a190c026b2f8094c2917bfae06114750d4be569c
major <echolabstech@gmail.com>
2021-06-22 02:13:06 -0400

    change page background when slider moves left and right

--- slider/src/App.jsx
+++ slider/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { makeStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
   app: {
@@ -22,6 +23,20 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  page: {
+    background: 'red',
+    width: '100%',
+    height: '100%',
+  },
+  page1: {
+    background: 'red',
+  },
+  page2: {
+    background: 'blue',
+  },
+  page3: {
+    background: 'green',
+  },
 }));
 
 function App(props) {
@@ -30,13 +45,24 @@ function App(props) {
   }
   const classes = useStyles();
   const [value, setValue] = React.useState(30);
+  const [page, setPage] = React.useState(1);
 
   function handleChangeCommited(event, newValue) {
     if (value !== newValue) {
       if (newValue < 50) {
         console.log('page slide right');
+        if (page >= 1) {
+          setPage(page - 1);
+        } else {
+          setPage(3);
+        }
       } else {
         console.log('page slide left');
+        if (page <= 3) {
+          setPage(page + 1);
+        } else {
+          setPage(1);
+        }
       }
       console.log('set value to ', 100 - newValue);
       setValue(newValue);
@@ -46,11 +72,27 @@ function App(props) {
   return (
     <div className={classes.app}>
       <div className={classes.screen}>
+        <div
+          className={clsx(
+            classes.page,
+            page === 1 ? classes.page1 : null,
+            page === 2 ? classes.page2 : null,
+            page === 3 ? classes.page3 : null,
+          )}
+        ></div>
         <Slider
           defaultValue={30}
           onChangeCommitted={handleChangeCommited}
           aria-labelledby="continuous-slider"
         />
+        <div
+          className={clsx(
+            classes.page,
+            page === 1 ? classes.page1 : null,
+            page === 2 ? classes.page2 : null,
+            page === 3 ? classes.page3 : null,
+          )}
+        ></div>
       </div>
     </div>
   );
