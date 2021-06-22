bec269c25b989263c6595aae1a989ec06e1a7076
major <echolabstech@gmail.com>
2021-06-21 21:42:23 -0400

    drop in page slider placeholder

--- slider/src/App.jsx
+++ slider/src/App.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
 import { makeStyles } from '@material-ui/core/styles';
+import Slider from '@material-ui/core/Slider';
 
 const useStyles = makeStyles((theme) => ({
   app: {
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     width: '100vw',
@@ -13,6 +16,11 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     height: '100%',
     border: '1px solid black',
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 }));
 
@@ -21,10 +29,30 @@ function App(props) {
     ...props
   }
   const classes = useStyles();
+  const [value, setValue] = React.useState(30);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeCommited = (event, newValue) => {
+    if (newValue < 50) {
+      console.log('slide right');
+    } else {
+      console.log('slide left');
+    }
+  };
 
   return (
     <div className={classes.app}>
-      <div className={classes.screen}></div>
+      <div className={classes.screen}>
+        <Slider
+          value={value}
+          onChange={handleChange}
+          onChangeCommitted={handleChangeCommited}
+          aria-labelledby="continuous-slider"
+        />
+      </div>
     </div>
   );
 }
