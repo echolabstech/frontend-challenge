02f53bfdaaddce3ef28f13e5c7ba479e7d85be5c
major <echolabstech@gmail.com>
2021-06-22 23:39:10 -0400

    stub onclick handler for tile clicks

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -5,8 +5,21 @@
 	- each tile has a random number in it, [1-8]
 	- the puzzle board always has 1 empty space
 */
-(function() {
+(() => {
+	function moveTile(event) {
+		/*
+			- identify tile clicked
+			- find tile neighbors
+			- is tile neighbor 0?
+			 - yes: swap tile and tile neighbor 0
+			 - no: do nothing
+		*/
+		console.log('move tile ', event.currentTarget.innerText);
+	}
+
 	const puzzle = document.querySelector('.puzzle-figure');
+	const tiles = document.querySelectorAll('.tile');
+	tiles.forEach(tile => {tile.onclick = moveTile;});
 	const board = [
 		[8, 5, 6],
 		[3, 1, 7],
