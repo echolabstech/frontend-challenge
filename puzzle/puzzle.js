5d4d7789e25e7dea6fe499fee2cf111a273ec4f0
major <echolabstech@gmail.com>
2021-06-22 22:47:43 -0400

    comment game rules

--- puzzle/puzzle.js
+++ puzzle/puzzle.js
@@ -1,3 +1,10 @@
+/*
+	Rules:
+	- if a tile has no free spaces around it, do nothing
+	- if a tile has a free space next to it, move the tile into that space
+	- each tile has a random number in it, [1-8]
+	- the puzzle board always has 1 empty space
+*/
 (function() {
 	const puzzle = document.querySelector('.puzzle-figure');
 	console.log(puzzle);
